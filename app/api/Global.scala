0ea555b004c047a1611b1fb3296e9a3c84ea9d92
Mikolaj Szabo <savingprivateblue@gmail.com>
2015-09-09 15:11:47 -0400

    id migration to new format #12

--- app/api/Global.scala
+++ app/api/Global.scala
@@ -11,6 +11,8 @@ import akka.actor.ActorSystem
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.inject.ApplicationLifecycle
 
+import scala.collection.JavaConversions._
+
 import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -44,6 +46,51 @@ class Global @javax.inject.Inject() (lifecycle: ApplicationLifecycle) {
                 case e: Throwable => logger.error(s"Database initialization failed: ${e.getMessage}")
             }
     Await.ready(runInit, Duration.Inf)
+
+    val oneOff = for {
+        _ <- Query.lift { db =>
+            val nodes = db.findNodes(Label.Block)
+            while(nodes.hasNext) {
+                val node = nodes.next()
+                val key = node.getProperty(Prop.BlockId.name).asInstanceOf[String]
+                val uuid = java.util.UUID.fromString(key)
+                val newId = IdGenerator.encode(uuid)
+                node.setProperty(Prop.BlockId.name, newId)
+                logger.info(s"Changing block id from $key to $newId")
+            }
+        }
+
+        _ <- Query.lift { db =>
+            val nodes = db.findNodes(Label.User)
+            while(nodes.hasNext) {
+                val node = nodes.next()
+                val key = node.getProperty(Prop.UserId.name).asInstanceOf[String]
+                val uuid = java.util.UUID.fromString(key)
+                val newId = IdGenerator.encode(uuid)
+                node.setProperty(Prop.UserId.name, newId)
+                logger.info(s"Changing user id from $key to $newId")
+            }
+        }
+
+        _ <- Query.lift { db =>
+            val rels = org.neo4j.tooling.GlobalGraphOperations.at(db).getAllRelationships().iterator
+            while(rels.hasNext) {
+                val rel = rels.next()
+                val key = rel.getProperty(Prop.UserId.name).asInstanceOf[String]
+                val uuid = java.util.UUID.fromString(key)
+                val newId = IdGenerator.encode(uuid)
+                rel.setProperty(Prop.BlockId.name, newId)
+                logger.info(s"Changing relationship user id from $key to $newId")
+            }
+        }
+    } yield ()
+    val runOneOff =
+        neo.run(oneOff)
+            .map(_ => logger.info("One-off maintenance task finished"))
+            .recover {
+                case e: Throwable => logger.error(s"One-off maintenance task failed failed: ${e.getMessage}")
+            }
+    Await.ready(runOneOff, Duration.Inf)
 
     lifecycle.addStopHook { () =>
         neo.shutdown()
