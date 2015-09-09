19b64dd8c087892d9980514841174acb2aceeeaa
Mikolaj Szabo <savingprivateblue@gmail.com>
2015-09-09 00:27:48 -0400

    moving logic from NeoModel to read controller

--- app/api/NeoModel.scala
+++ app/api/NeoModel.scala
@@ -2,14 +2,6 @@ package api
 
 import model._
 
-import org.neo4j.graphdb.Node
-import org.neo4j.graphdb.Relationship
-import org.neo4j.graphdb.Direction
-
-import scalaz._
-
-import scala.collection.JavaConversions._
-
 object NeoModel {
     object Label {
         val Block = neo.Label("Block")
@@ -35,58 +27,4 @@ object NeoModel {
         val Follow = neo.Arrow("FOLLOW")
         val Block = neo.Arrow("BLOCK")
     }
-
-    // TODO validation if all properties are present
-    def relationshipToConnection(base: Node, rel: Relationship) = Connection(
-        userId = UserId(rel.getProperty(Prop.UserId.name).asInstanceOf[String]),
-        timestamp = rel.getProperty(Prop.Timestamp.name).asInstanceOf[Long],
-        arrow = neo.Arrow(rel.getType.name),
-        blockId = {
-            val node = rel.getOtherNode(base)
-            BlockId(node.getProperty(Prop.BlockId.name).asInstanceOf[String])
-        }
-    )
-
-    // TODO validation if node is really a user node with all properties present
-    def nodeToUser(node: Node) = User(
-        userId = UserId(node.getProperty(Prop.UserId.name).asInstanceOf[String]),
-        timestamp = node.getProperty(Prop.Timestamp.name).asInstanceOf[Long],
-        foreignId = node.getProperty(Prop.UserForeignId.name).asInstanceOf[String],
-        name = node.getProperty(Prop.UserName.name).asInstanceOf[String]
-    )
-
-    // TODO validation if node is really a block node with all properties present
-    def nodeToBlock(node: Node) = Block(
-        blockId = BlockId(node.getProperty(Prop.BlockId.name).asInstanceOf[String]),
-        title = \/.fromTryCatchNonFatal {
-            node.getProperty(Prop.BlockTitle.name).asInstanceOf[String]
-        }.toOption,
-        timestamp = node.getProperty(Prop.Timestamp.name).asInstanceOf[Long],
-        body = {
-            val bodyType = node.getProperty(Prop.BlockBodyType.name).asInstanceOf[String]
-            bodyType match {
-                case BlockBody.Type.text =>
-                    Text(node.getProperty(Prop.BlockBody.name).asInstanceOf[String])
-                case BlockBody.Type.image =>
-                    Image(node.getProperty(Prop.BlockBody.name).asInstanceOf[String])
-            }
-        },
-        author = {
-            // TODO null check
-            val rel = node.getSingleRelationship(Arrow.Author, Direction.INCOMING)
-            nodeToUser(rel.getStartNode)
-        },
-        incoming = {
-            node.getRelationships(Direction.INCOMING)
-                .filter(_.getOtherNode(node).getLabels.toSeq.contains(Label.Block))
-                .map(rel => relationshipToConnection(node, rel))
-                .toSeq
-        },
-        outgoing = {
-            node.getRelationships(Direction.OUTGOING)
-                .filter(_.getOtherNode(node).getLabels.toSeq.contains(Label.Block))
-                .map(rel => relationshipToConnection(node, rel))
-                .toSeq
-        }
-    )
 }
