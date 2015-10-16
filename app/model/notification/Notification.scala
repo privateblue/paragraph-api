7593482fcd60242a34c66a7fe34cadaa9cb6fd09
Mikolaj Szabo <savingprivateblue@gmail.com>
2015-10-16 18:43:56 -0400

    creating and sending notifications about blocks #14

---
+++ app/model/notification/Notification.scala
@@ -0,0 +1,60 @@
+package model.notification
+
+import model.base.BlockId
+import model.base.UserId
+
+import redis.ByteStringSerializer
+import redis.ByteStringDeserializer
+
+import play.api.libs.json._
+
+sealed trait Notification {
+    def label = this match {
+        case PathNotification(_, _, _, _, _) => Notification.Label.path
+        case UserNotification(_, _, _, _, _) => Notification.Label.user
+    }
+}
+
+case class PathNotification(
+    notificationId: NotificationId,
+    timestamp: Long,
+    userId: UserId,
+    text: String,
+    path: List[BlockId]
+) extends Notification
+
+case class UserNotification(
+    notificationId: NotificationId,
+    timestamp: Long,
+    userId: UserId,
+    text: String,
+    about: UserId
+) extends Notification
+
+object Notification {
+    object Label {
+        val path = "path"
+        val user = "user"
+    }
+
+    implicit val pathNotificationFormat = Json.format[PathNotification]
+
+    implicit val userNotificationFormat = Json.format[UserNotification]
+
+    implicit val notificationFormat = new Format[Notification] {
+        def reads(json: JsValue) = JsSuccess(
+            (json \ "type").as[String] match {
+                case Label.path => (json \ "notification").as[PathNotification]
+                case Label.user => (json \ "notification").as[UserNotification]
+            }
+        )
+        def writes(notification: Notification) = notification match {
+            case n @ PathNotification(_, _, _, _, _) => Json.obj("type" -> Label.path, "notification" -> n)
+            case n @ UserNotification(_, _, _, _, _) => Json.obj("type" -> Label.user, "notification" -> n)
+        }
+    }
+
+    implicit val notificationSerializer = implicitly[ByteStringSerializer[String]].contramap[Notification](Json.toJson(_).toString)
+
+    implicit val notificationDeserializer = implicitly[ByteStringDeserializer[String]].map(Json.parse(_).as[Notification])
+}
---
+++ app/model/notification/NotificationId.scala
@@ -0,0 +1,38 @@
+package model.notification
+
+import model.base.Id
+
+import redis.ByteStringSerializer
+import redis.ByteStringDeserializer
+
+import play.api.mvc._
+import play.api.libs.json._
+
+import scala.util.Right
+
+case class NotificationId(val key: String) extends Id[String] {
+    override def toString = key
+}
+
+object NotificationId {
+    implicit val notificationIdFormat = new Format[NotificationId] {
+        def reads(json: JsValue) = JsSuccess(NotificationId(json.as[String]))
+        def writes(id: NotificationId) = JsString(id.key)
+    }
+
+    implicit val pathBinder = new PathBindable[NotificationId] {
+        override def bind(key: String, value: String): Either[String, NotificationId] = Right(NotificationId(value))
+        override def unbind(key: String, notificationId: NotificationId): String = notificationId.key
+    }
+
+    implicit val queryStringBinder = new QueryStringBindable[Seq[NotificationId]] {
+        override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Seq[NotificationId]]] =
+            params.get(key).map(ids => Right(ids.map(apply)))
+        override def unbind(key: String, notificationIds: Seq[NotificationId]): String =
+            notificationIds.map(id => s"$key=$id").mkString("&")
+    }
+
+    implicit val notificationIdSerializer = implicitly[ByteStringSerializer[String]].contramap[NotificationId](_.key)
+
+    implicit val notificationIdDeserializer = implicitly[ByteStringDeserializer[String]].map(NotificationId.apply)
+}
