ee975aa0659f53940133d7312b2b8c6a2a9353e9
Mikolaj Szabo <savingprivateblue@gmail.com>
2015-10-17 15:48:30 -0400

    the notification api #14

--- app/api/notification/Notifications.scala
+++ app/api/notification/Notifications.scala
@@ -4,18 +4,16 @@ import model.base.UserId
 import model.base.BlockId
 import model.notification._
 
-import api.base._
-import api.messaging.Messages
+import api.base.IdGenerator
 
-import akka.actor.ActorSystem
-import akka.stream.Materializer
+import redis._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
 object Notifications {
-    def notifyAboutBlock(userId: UserId, timestamp: Long, text: String, from: BlockId, to: BlockId)(implicit ec: ExecutionContext): redis.Command.Exec[Notification] =
-        redis.Command { redis =>
+    def notifyAboutBlock(userId: UserId, timestamp: Long, text: String, from: BlockId, to: BlockId)(implicit ec: ExecutionContext): Command.Exec[Notification] =
+        Command { redis =>
             val notificationId = NotificationId(IdGenerator.key)
             val notification = PathNotification(
                 notificationId = notificationId,
@@ -33,7 +31,24 @@ object Notifications {
             }
         }
 
-    def noop = redis.Command(_ => Future.successful(()))
+    def noop = Command(_ => Future.successful(()))
+
+    def get(userId: UserId)(implicit ec: ExecutionContext): Command.Exec[Seq[Notification]] =
+        Command { redis =>
+            for {
+                ids <- redis.smembers[String](key(userId))
+                notifications <- if (ids.isEmpty) Future.successful(List())
+                                 else redis.mget[Notification](ids:_*)
+            } yield notifications.flatten.sorted
+        }
+
+    def dismiss(notificationId: NotificationId, userId: UserId)(implicit ec: ExecutionContext): Command.Exec[Unit] =
+        Command { redis =>
+            for {
+                _ <- redis.srem[NotificationId](key(userId), notificationId)
+                _ <- redis.del(notificationId.toString)
+            } yield ()
+        }
 
     private def key(userId: UserId): String = s"$userId-notifications"
 }
