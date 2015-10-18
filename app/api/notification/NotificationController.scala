0e446b6551aa3692f4b5dd4ac9e281b7e679f1ae
Mikolaj Szabo <savingprivateblue@gmail.com>
2015-10-18 09:54:47 -0400

    notification messaging #14

--- app/api/notification/NotificationController.scala
+++ app/api/notification/NotificationController.scala
@@ -2,8 +2,11 @@ package api.notification
 
 import api._
 import api.base._
+import api.messaging.Messages
 
 import model.notification._
+
+import akka.stream.scaladsl.Source
 
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -17,6 +20,19 @@ class NotificationController @javax.inject.Inject() (implicit global: api.Global
     def dismiss = Actions.authenticated { (userId, timestamp, body) =>
         val notificationId = (body \ "notificationId").as[NotificationId]
         val prg = Notifications.dismiss(notificationId, userId).program
+        Program.run(prg, global.env)
+    }
+
+    implicit val kafkaSystem = global.kafkaSystem
+    implicit val kafkaMaterializer = global.kafkaMaterializer
+
+    def subscribe = Actions.authenticatedSocket[Notification] { userId =>
+        val prg = Messages.listen[Notification, Source[Notification, _]]("notification") { source =>
+            source.collect {
+                case n @ PathNotification(_, _, addresseeId, _, _) if addresseeId == userId => n
+                case n @ UserNotification(_, _, addresseeId, _, _) if addresseeId == userId => n
+            }
+        }.program
         Program.run(prg, global.env)
     }
 }
