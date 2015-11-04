a3dda4a1ee460e63c7153c65a0df403afe5764e4
Mikolaj Szabo <savingprivateblue@gmail.com>
2015-11-04 16:59:59 -0500

    validation added to external page parser #17

--- app/api/external/Pages.scala
+++ app/api/external/Pages.scala
@@ -1,8 +1,11 @@
 package api.external
 
+import api.base._
+
 import model.external._
 
 import org.jsoup._
+import org.jsoup.nodes.Element
 
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
@@ -17,27 +20,64 @@ import scala.concurrent.ExecutionContext
 
 object Pages {
     def parse(url: String)(implicit ec: ExecutionContext, mat: Materializer) = for {
-        page <- load(url)
-    } yield {
-        val doc = Jsoup.parse(page)
-        val ps = doc.select("article p:not(aside p):not(:has(small))").toList
-        val paragraphs = ps.foldLeft(List.empty[Paragraph]) { (list, p) =>
-            val links = p.select("a").map(_.attr("href")).toList
-            val title = Option(p.previousElementSibling).flatMap { prev =>
-                if (Set("h1", "h2", "h3", "h4", "h5", "h6").contains(prev.tag.getName)) Some(prev.text)
-                else None
-            }
-            if (p.hasText) Text(title, p.text, links) :: list
-            else p.select("img").map(img => Image(title, img.attr("src"), links)).toList ++ list
-        }.reverse
-        val url = doc.select("meta[name=og:url]").first.attr("content")
-        val author = doc.select("meta[name=author]").first.attr("content")
-        val title = doc.select("meta[property=og:title]").first.attr("content")
-        val site = doc.select("meta[property=og:site_name]").first.attr("content")
-        Page(url, author, title, site, paragraphs)
-    }
+        html <- load(url)
+        doc = Jsoup.parse(html)
+    } yield validate(page(doc))
 
     private def load(url: String)(implicit ec: ExecutionContext, mat: Materializer) = http.Command.get(url) {
         case HttpResponse(_, _, entity, _) => Unmarshal(entity).to[String]
     }
+
+    private def page(elem: Element): ValidationNel[Throwable, Page] = {
+        (canonicalUrlOf(elem) |@| authorOf(elem) |@| titleOf(elem) |@| siteOf(elem)) {
+            case (url, author, title, site) => Page(url, author, title, site, paragraphsOf(elem))
+        }
+    }
+
+    private def canonicalUrlOf(elem: Element) = metaOf(elem, "meta[property=og:url]")
+
+    private def authorOf(elem: Element) = metaOf(elem, "meta[name=author]")
+
+    private def titleOf(elem: Element) = metaOf(elem, "meta[property=og:title]")
+
+    private def siteOf(elem: Element) = metaOf(elem, "meta[property=og:site_name]")
+
+    private def metaOf(elem: Element, selector: String): ValidationNel[Throwable, String] =
+        Validation.fromTryCatchNonFatal[String] {
+            val n = elem.select(selector).first
+            if (n != null && n.attr("content") != "") n.attr("content")
+            else throw ApiError(500, s"$selector not found")
+        }.toValidationNel
+
+    private def paragraphsOf(elem: Element): List[Paragraph] = {
+        val ps = elem.select("article p:not(aside p):not(:has(small))").toList
+        ps.foldLeft(List.empty[Paragraph]) { (list, p) =>
+            val links = linksOf(p)
+            val heading = headingOf(p)
+            if (p.hasText) Paragraph.Text(heading, p.text, links) :: list
+            else imagesOf(p).map(Paragraph.Image(heading, _, links)) ++ list
+        }.reverse
+    }
+
+    private val linkPattern = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"
+
+    private def linksOf(elem: Element): List[String] =
+        elem.select("a").flatMap { n =>
+            val href = n.attr("abs:href")
+            if (href.matches(linkPattern)) Some(href) else None
+        }.toList
+
+    private def headingOf(elem: Element): Option[String] =
+        Option(elem.previousElementSibling).flatMap { prev =>
+            if (Set("h1", "h2", "h3", "h4", "h5", "h6").contains(prev.tag.getName)) Some(prev.text)
+            else None
+        }
+
+    private val imagePattern = "([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)"
+
+    private def imagesOf(elem: Element): List[String] =
+        elem.select("img").flatMap { n =>
+            val src = n.attr("src")
+            if (src.matches(imagePattern)) Some(src) else None
+        }.toList
 }
--- app/model/external/Paragraph.scala
+++ app/model/external/Paragraph.scala
@@ -1,5 +1,8 @@
 package model.external
 
 sealed trait Paragraph
-case class Text(title: Option[String], content: String, links: List[String]) extends Paragraph
-case class Image(title: Option[String], url: String, links: List[String]) extends Paragraph
+
+object Paragraph {
+    case class Text(heading: Option[String], content: String, links: List[String]) extends Paragraph
+    case class Image(heading: Option[String], url: String, links: List[String]) extends Paragraph
+}
