cc0b902c433c2988cba51ddedbc4840419d43dc2
Mikolaj Szabo <savingprivateblue@gmail.com>
2016-01-24 01:27:27 -0500

    improvements to neo dsl string interpolation

--- app/neo/package.scala
+++ app/neo/package.scala
@@ -3,28 +3,28 @@ import scala.collection.JavaConverters._
 package object neo {
     implicit class QueryHelper(val sc: StringContext) extends AnyVal {
         def neo(args: Any*): Query = {
-            val (_, query, parameters, _) =
-                sc.parts.tail.foldLeft((args.toList, sc.parts.head, Map.empty[String, AnyRef], 0)) {
-                    case ((Label(name)::rest, q, params, c), cur) =>
-                        (rest, s"$q$name$cur", params, c)
+            val (_, query, parameters) =
+                sc.parts.tail.foldLeft((args.toList, sc.parts.head, Map.empty[String, java.lang.Object])) {
+                    case ((Label(name)::rest, q, params), cur) =>
+                        (rest, s"$q$name$cur", params)
 
-                    case ((Arrow(name)::rest, q, params, c), cur) =>
-                        (rest, s"$q$name$cur", params, c)
+                    case ((Arrow(name)::rest, q, params), cur) =>
+                        (rest, s"$q$name$cur", params)
 
-                    case ((Property(name, identifier)::rest, q, params, c), cur) =>
+                    case ((Property(name, identifier)::rest, q, params), cur) =>
                         val id = identifier.map(i => s"$i.").getOrElse("")
-                        (rest, s"$q$id$name$cur", params, c)
+                        (rest, s"$q$id$name$cur", params)
 
-                    case ((PropertyValue.NonEmpty(identifier, name, value)::rest, q, params, c), cur) =>
-                        val pName = s"p$c"
+                    case ((PropertyValue.NonEmpty(identifier, name, value)::rest, q, params), cur) =>
+                        val pName = s"p${params.size}"
                         val expr = identifier.map(i => s"$i.$name={$pName}").getOrElse(s"$name:{$pName}")
-                        (rest, s"$q$expr$cur", params + (pName -> value), c + 1)
+                        (rest, s"$q$expr$cur", params + (pName -> value))
 
-                    case ((PropertyValue.Empty::rest, q, params, c), cur) =>
-                        (rest, s"$q[[[empty]]]$cur", params, c)
+                    case ((PropertyValue.Empty::rest, q, params), cur) =>
+                        (rest, s"$q[[[empty]]]$cur", params)
 
-                    case ((other::rest, q, params, c), cur) =>
-                        (rest, s"$q$other$cur", params, c)
+                    case ((other::rest, q, params), cur) =>
+                        (rest, s"$q$other$cur", params)
                 }
             val sanitized = query
                 .replaceAll("\n", "")
